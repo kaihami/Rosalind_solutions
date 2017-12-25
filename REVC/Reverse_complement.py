0d1ef476afd12c2dda7baa836a6fcfa425b0bbd4
kaihami <kaihami@gmail.com>
2017-12-24 19:19:38 -0500

    Create Reverse_complement.py

---
+++ REVC/Reverse_complement.py
@@ -0,0 +1,30 @@
+'''
+My solution to Rosalind Bioinformatics Problem
+Title: Complementing a Strand of DNA
+Rosalind ID: REVC
+Rosalind #: 003
+URL: http://rosalind.info/problems/revc/
+'''
+
+import sys
+
+def Reverse_Complement(fi):
+    '''
+    :return: The transcribed RNA string.
+    '''
+    ipt_file = open(fi, 'r').read().replace('\n', '')
+
+    complement = {'A': 'T', 'T':'A', 'C':'G', 'G':'C'}
+
+    print 'Original:          ', ipt_file
+
+    rev_complement = ''.join([complement[x] for x in ipt_file[::-1]])
+
+    print 'Reverse Complement:', rev_complement
+
+    output_name = 'output_'+fi
+
+    with open(output_name, 'a') as f:
+        f.write(rev_complement)
+
+Reverse_Complement(sys.argv[1])
