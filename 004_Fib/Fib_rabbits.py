7e9fd43bbcc7cffdda4860e1e00ad73853ec7650
kaihami <kaihami@gmail.com>
2017-12-25 15:18:36 -0500

    Create Fib_rabbits.py

---
+++ 004_Fib/Fib_rabbits.py
@@ -0,0 +1,37 @@
+'''
+My solution to Rosalind Bioinformatics Problem
+Title: Complementing a Strand of DNA
+Rosalind ID: Fib
+Rosalind #: 004
+URL: http://rosalind.info/problems/fib/
+'''
+
+import sys
+
+# A memoization solution for n, k rabbits problem
+# Fast solution, uses more memory
+def fib(n,k):
+    '''
+    :param n: number of months
+    :param k: each pair of rabbits produces k-litters
+    :return: number of rabbits.
+    '''
+    def fib_memo(n,k,cache):
+        if n in cache:
+            return cache[n]
+        answer = fib_memo(n - 1, k,cache) + (fib_memo(n - 2,k, cache)*k)
+        cache[n] = answer
+        return answer
+    cache = {1: 1, 2: 1}
+    return fib_memo(n,k, cache)
+
+def Wrap(fi):
+
+    n, k = [int(x) for x in open(fi, 'r').read().replace('\n', ''). split(' ')]
+
+    output_name = 'output_'+fi
+    print 'Total number of Rabbits:', str(fib(n,k))
+    with open(output_name, 'a') as f:
+        f.write(str(fib(n,k)))
+
+Wrap(sys.argv[1])
